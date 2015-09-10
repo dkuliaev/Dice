3fa0bd64d19989c2d2c2a798b42e255ccf0f3126
dkuliaev <dkuliaev@gmail.com>
2015-09-10 11:35:04 -0700

    finish

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,10 +1,26 @@
-void setup()
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
+public void setup()
 {
   noLoop();
   size(500, 500);
   background(200);
 }
-void draw()
+public void draw()
 {
   background(200);
 
@@ -18,7 +34,7 @@ void draw()
     }
   }
 }
-void mousePressed()
+public void mousePressed()
 {
   redraw();
 }
@@ -36,11 +52,11 @@ class Die
   }
 
 
-  void roll()
+  public void roll()
   {
     value = (int)(Math.random() * 6 + 1);
   }
-  void show()
+  public void show()
   {
     
 
@@ -106,5 +122,14 @@ class Die
       ellipse(dieX + 25, dieY + 50, 15, 15);
       ellipse(dieX + 75, dieY + 50, 15, 15);
     } 
+  }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
 }
